e95eb58e10b1465201aaf4eeae14c091ec644e43
濵口聖弥 <2410038@i-seifu.jp>
2025-09-12 14:54:17 +0900

    当たり判定完成前

--- 3D_Base/Source/Collision/CollisionManager/CCollisionManager.h
+++ 3D_Base/Source/Collision/CollisionManager/CCollisionManager.h
@@ -5,23 +5,25 @@
 
 //-----外部クラス-----
 #include "Collision//Collider//CCollider.h" // コライダークラス
+#include "GameObject//CGameObject.h" // ゲームオブジェクトクラス
 
 //===================================
 //	コリジョンマネージャークラス
 //===================================
 class CCollisionManager
 {
+public:
+	void AddCollider(std::shared_ptr<CCollider> collider)
+	{
+		m_Colliders.push_back(collider);
+	}
+
 private:
 
 	static CCollisionManager& Instance()
 	{
 		static CCollisionManager instance;
 		return instance;
-	}
-
-	void AddCollider(std::shared_ptr<CCollider> collider)
-	{
-		m_Colliders.push_back(collider);
 	}
 
 	void Clear()
