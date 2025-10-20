fb21c68c29760f6a1264167b701c71bca5b97604
濵口聖弥 <2410038@i-seifu.jp>
2025-10-20 18:18:47 +0900

    弾の修正

--- 3D_Base/Source/GameObject/StaticMeshObject/Shot/CShot.cpp
+++ 3D_Base/Source/GameObject/StaticMeshObject/Shot/CShot.cpp
@@ -25,21 +25,6 @@ void CShot::Initialize(int id)
 
 void CShot::Update()
 {
-	//時間定数宣言.
-	const float TIME = 1.0f / FPS;
-
-	if (m_Shot.m_Display == true)
-	{
-		// 移動方向に移動速度をかけ合わせたものを座標に反映
-		m_vPosition += m_Shot.m_MoveDirection * m_Shot.m_MoveSpeed * TIME;
-
-		if (m_Shot.m_DisplayTime <= 120)
-		{
-			// 加速度に重力が与えられていく
-			m_Shot.m_Velocity += m_Shot.m_Gravity * TIME;
-			// 加速度にYを与える
-			m_vPosition.y += m_Shot.m_Velocity * TIME;
-		}
 	// コライダーの座標を更新
 	m_pCollider->SetPosition(m_vPosition);
 
@@ -65,32 +50,21 @@ void CShot::Update()
 		if (m_Shot.m_DisplayTime < 0) {
 			//見えない所に置いておく
 			m_vPosition = D3DXVECTOR3(0.f, -10.f, 0.f);
-			m_Display = false;
+			m_Shot.m_Display = false;
 		}
 	}
-
 }
 
 void CShot::Draw(D3DXMATRIX& View, D3DXMATRIX& Proj, LIGHT& Light, CAMERA& Camera)
 {
-	if (!m_Display) return;
-	CStaticMeshObject::Draw(View, Proj, Light, Camera);
+	if (m_Shot.m_Display == true)
+	{
+		CStaticMeshObject::Draw(View, Proj, Light, Camera);
+	}
 }
 
 void CShot::Reload(const D3DXVECTOR3& Pos, float RotY)
 {
-	//弾を新規発射として初期化
-	m_vPosition = Pos;
-	m_vRotation.y = RotY;		// 弾の向き(見た目)も変える
-
-	//前方をY回転rotYで回した方向を移動方向にする
-	m_MoveDirection = D3DXVECTOR3(std::sinf(RotY), 0.0f, std::cosf(RotY));	//正規化
-	m_VelocityY = 0.0f;
-
-	m_LifeFrames = (m_LifeFramesInit > 0) ? m_LifeFramesInit : 120;
-	m_Display = true;
-
-#if 0
 	for (int i = 0; i < ShotMax; i++)
 	{
 		//弾が発射されていたら戻す.
@@ -99,15 +73,32 @@ void CShot::Reload(const D3DXVECTOR3& Pos, float RotY)
 		// 回転に応じた発射位置を計算
 		D3DXVECTOR3 offset = { 0.f, 0.3f, 1.5f }; // 砲塔の先端に合わせる
 		D3DXMATRIX mRotationY;
-		// Y軸回転行列を作成
-		D3DXMatrixRotationY(
-			&mRotationY,	// (out)行列
-			m_vRotation.y);	// プレイヤーのY方向の回転値
+		D3DXMatrixRotationY(&mRotationY, RotY);
+
+		D3DXVECTOR3 rotatedOffset;
+		D3DXVec3TransformCoord(&rotatedOffset, &offset, &mRotationY);
+
+		// プレイヤー位置に加算して弾の位置決定
+		m_vPosition = Pos + rotatedOffset;
+
+		// 弾の回転（見た目用）
+		m_vRotation.y = RotY;
+
+		m_Shot.m_Display = true;
+		m_Shot.m_Velocity = 0.5f;
+		m_Shot.m_DisplayTime = FPS * 3;
 
 		// 弾の進行方向（Z軸を回転させる）
 		m_Shot.m_MoveDirection = D3DXVECTOR3(0.f, 0.f, 1.f);
 		D3DXVec3TransformCoord(&m_Shot.m_MoveDirection, &m_Shot.m_MoveDirection, &mRotationY);
 	}
-#endif
 }
 
+bool CShot::IsActive() const
+{
+	if (m_Shot.m_Display)
+	{
+		return true;
+	}
+	return false;
+}
--- 3D_Base/Source/GameObject/StaticMeshObject/Shot/CShot.h
+++ 3D_Base/Source/GameObject/StaticMeshObject/Shot/CShot.h
@@ -1,9 +1,7 @@
 #pragma once
 #include "GameObject//StaticMeshObject//CStaticMeshObject.h" /* 継承クラス || スタティックメッシュオブジェクトクラス */
-
 /**************************************************
-*	弾クラス(単発)
-* ShotManagerがプレイヤーごとに複数個所持する
+*	弾クラス
 **/
 class CShot
 	: public CStaticMeshObject
@@ -19,18 +17,8 @@ public:
 	//弾を再設定
 	void Reload(const D3DXVECTOR3& Pos, float RotY);
 
-	void SetDisplay(bool disp) { m_Display = disp; }
-	bool IsActive() const { return m_Display; }
-
-private:
-	bool		m_Display = false;		// 表示切替
-	D3DXVECTOR3 m_MoveDirection;		// 移動方向
-	float		m_MoveSpeed = 0.5f;		// 移動速度
-	int 		m_LifeFramesInit = 120;	// 約3秒くらい表示する
-	int			m_LifeFrames = 0;		// フレームカウンタ
-	float 		m_Gravity = 3.0f;		// 弾の重力
-	float 		m_VelocityY = 1.0f;		// 加速度
-
+	void SetDisplay(bool disp) { m_Shot.m_Display = disp; }
+	bool IsActive() const;
 protected:
 	//弾構造体.
 	struct Shot
