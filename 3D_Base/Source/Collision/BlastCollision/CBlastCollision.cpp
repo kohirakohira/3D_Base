e51af3b7fdee676767194a813a130f2879ce4736
Matsuoka <2400005@i-seifu.ac.jp>
2025-10-22 13:41:08 +0900

    爆風の当たり判定の追加

---
+++ 3D_Base/Source/Collision/BlastCollision/CBlastCollision.cpp
@@ -0,0 +1,9 @@
+#include "CBlastCollision.h"
+
+CBlastCollision::CBlastCollision()
+{
+}
+
+CBlastCollision::~CBlastCollision()
+{
+}
---
+++ 3D_Base/Source/Collision/BlastCollision/CBlastCollision.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+//”š”­‚ÌƒNƒ‰ƒX.
+class CBlastCollision
+{
+public:
+	CBlastCollision();
+	~CBlastCollision();
+
+private:
+
+};
