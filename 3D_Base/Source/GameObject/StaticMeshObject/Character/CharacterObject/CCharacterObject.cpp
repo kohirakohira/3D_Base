80ea59b3775a93bf630a55f4f636c00aa4e762de
kohirakohira <2400067@i-seifu.ac.jp>
2025-11-13 08:01:31 +0900

    変更1

--- 3D_Base/Source/GameObject/StaticMeshObject/Character/CharacterObject/CCharacterObject.cpp
+++ 3D_Base/Source/GameObject/StaticMeshObject/Character/CharacterObject/CCharacterObject.cpp
@@ -9,14 +9,47 @@ CCharacterObjectBase::CCharacterObjectBase(
 	: m_HP					( hp )
 	, m_MaxHP				( hp )
 	, m_Tuning				( tuning )
-	, m_Body				( body )
-	, m_Cannon				( cannon )
+	, m_Body				(std::move(body))
+	, m_Cannon				(std::move(cannon))
 	, m_IsActive			( false )
 	, m_IsAlive				( false )
 	, m_Drawflag			( false )
 {
 }
 
-CCharacterObjectBase::~CCharacterObjectBase()
+CCharacterObjectBase::~CCharacterObjectBase() = default;
+
+
+void CCharacterObjectBase::Create(int index)
 {
+	//Šî’ê‚Å‚Í‚È‚É‚à‚µ‚È‚¢.‹ïÛ‚Å¶¬Ó–±‚ðŽ‚½‚¹‚é
 }
+
+void CCharacterObjectBase::Update()
+{
+	if (m_Body)
+	{
+		m_Body->Update();
+	}
+
+	if (m_Cannon)
+	{
+		m_Cannon->Update();
+	}
+}
+
+void CCharacterObjectBase::Draw(D3DXMATRIX& View, D3DXMATRIX& Proj, LIGHT& Light, CAMERA& Camera)
+{
+	if (!m_Drawflag) return;
+
+	if (m_Body)
+	{
+		m_Body->Draw(View, Proj, Light, Camera);
+	}
+
+	if (m_Cannon)
+	{
+		m_Cannon->Draw(View, Proj, Light, Camera);
+	}
+}
+
