b92d16299dd2d7cef13a2ccd6fee0da3baa1d4d3
Matsuoka <2400005@i-seifu.ac.jp>
2025-11-17 18:43:20 +0900

    地面の設定

--- 3D_Base/Source/SceneManager/SceneBase/GameTitle/TitleProduction/CTitleProduction.cpp
+++ 3D_Base/Source/SceneManager/SceneBase/GameTitle/TitleProduction/CTitleProduction.cpp
@@ -3,7 +3,9 @@
 CTitleProduction::CTitleProduction()
 	: m_Camera				( nullptr )
 	, m_GroundMesh			( nullptr )
+	, m_BackGroundMesh		( nullptr )
 
+	, m_pBackImgObject		( nullptr )
 	, m_Ground				( nullptr )
 {
 }
@@ -19,21 +21,20 @@ void CTitleProduction::Update()
 	m_Camera->Update();
 
 	//地面を動かす.
-	m_Ground->Move(3.0f);
+	m_Ground->Update();
 }
 
 //描画関数.
 void CTitleProduction::Draw()
 {
-	//ライトの設定
-	m_Camera->SetLightPos(30.f, 70.f, 0.f);	//ポジション
-	m_Camera->SetLightIntensity(150.f);		//ライトの強さ
-	m_Camera->SetLightRange(1e9);			//影響範囲.距離減衰
 	//カメラの情報更新.
 	m_Camera->Info();
 
 	//地面を描画する.
 	m_Ground->Draw(m_Camera->m_mView, m_Camera->m_mProj, m_Camera->m_Light, m_Camera->m_Camera);
+	//背景を描画する.
+	m_pBackImgObject->Draw(m_Camera->m_mView, m_Camera->m_mProj, m_Camera->m_Light, m_Camera->m_Camera);
+
 }
 
 //生成関数.
@@ -43,10 +44,15 @@ void CTitleProduction::Create()
 	m_Camera = std::make_shared<CCamera>();
 
 	//地面の生成.
-	m_Ground = std::make_unique<CGround>();
+	m_Ground = std::make_unique<CGroundManager>();
+	m_Ground->Create();
+
+	//背景の生成.
+	m_pBackImgObject = std::make_unique<CStaticMeshObject>();
 
 	//スタティックメッシュの生成.
-	m_GroundMesh = std::make_shared<CStaticMesh>();
+	m_GroundMesh		= std::make_shared<CStaticMesh>();
+	m_BackGroundMesh	= std::make_shared<CStaticMesh>();
 
 }
 
@@ -55,26 +61,37 @@ void CTitleProduction::Init()
 {
 	//カメラの初期化.
 	m_Camera->Init();
-	
 	//カメラの設定.
-	m_Camera->SetLightPos(0.0f, 10.0f, 10.0f);		//位置設定.
+	m_Camera->SetLightPos(0.0f, 100.0f, 100.0f);	//位置設定.
 	m_Camera->SetLightColor(1.0f, 1.0f, 1.0f);		//色の設定.
-	m_Camera->SetLightIntensity(10.0f);				//ライトの強さ.
-	m_Camera->SetLightRange(100.0f);				//ライトの長さ.
+	m_Camera->SetLightIntensity(900.0f);			//ライトの強さ.
+	m_Camera->SetLightRange(1000.0f);				//ライトの長さ.
 	m_Camera->SetLightAtten(0.0f, 0.0f, 0.1);		//ライトの減衰.
+	m_Camera->SetCameraPos(0.0f, 100.0f, 0.0f);		//カメラ位置の設定.
 
 	//地面の初期化.
 	m_Ground->Init();
-	
+	m_Ground->SetPosition(0.0f, -38.0f, 0.0f);
+	m_Ground->SetRotato(0.0f, 0.0f, 0.0f);
+
+	//背景の初期化.
+	m_pBackImgObject->SetPosition(0.0f, 0.0f, 0.0f);
+	m_pBackImgObject->SetRotation(0.0f, 0.0f, 0.0f);
+	m_pBackImgObject->SetScale(28.0f, 28.0f, 28.0f);
 }
 
 //読み込み関数.
 HRESULT CTitleProduction::LoadData()
 {
 	//地面のメッシュの読み込み.
-	m_GroundMesh->Init(_T("Data\\Mesh\\Static\\Ground\\ground.x"));
+	m_GroundMesh->Init(_T("Data\\Mesh\\Static\\Ground\\groundex.x"));
+	//背景のメッシュの読み込み.
+	m_BackGroundMesh->Init(_T("Data\\Mesh\\Static\\OutBackImage\\BackImage.x"));
+
 	//地面メッシュのアタッチ.
 	m_Ground->AttachMesh(m_GroundMesh);
+	//背景メッシュのアタッチ.
+	m_pBackImgObject->AttachMesh(m_BackGroundMesh);
 
 	return S_OK;
 }
