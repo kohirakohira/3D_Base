51ee925be2d0af6b9266cabc8acaf9ca9f551715
Matsuoka <2400005@i-seifu.ac.jp>
2025-11-28 10:34:39 +0900

    マージ後での修正 ※アイテムボックスマネージャー

--- 3D_Base/Source/GameObject/StaticMeshObject/ItemBoxManager/CItemBoxManager.cpp
+++ 3D_Base/Source/GameObject/StaticMeshObject/ItemBoxManager/CItemBoxManager.cpp
@@ -75,15 +75,6 @@ void CItemBoxManager::AttachMesh(std::shared_ptr<CStaticMesh> pMesh)
 	m_ItemMesh = pMesh;
 }
 
-void CItemBoxManager::CreateBounding(std::shared_ptr<CStaticMesh>& pItem)
-{
-	// バウンディング設定.
-	for (auto& item : m_Item)
-	{
-		item->CreateBounding(pItem);
-	}
-}
-
 void CItemBoxManager::RemoveItem(int index)
 {
 	if (index >= 0 && index < m_Item.size())
