90d30e306d5aec0f2efee03b4da0b8126b7dd50f
Matsuoka <2400005@i-seifu.ac.jp>
2025-12-01 10:30:01 +0900

    地面の描画(仮)

--- 3D_Base/Source/SceneManager/SceneBase/GameTitle/TitleProduction/CTitleProduction.cpp
+++ 3D_Base/Source/SceneManager/SceneBase/GameTitle/TitleProduction/CTitleProduction.cpp
@@ -5,7 +5,8 @@ CTitleProduction::CTitleProduction()
 	, m_GroundMesh			( nullptr )
 	, m_PlayerMesh			( nullptr )
 
-	, m_Ground				( nullptr )
+	, m_SpriteGround		( nullptr )
+	, m_SpriteObjGround		( nullptr )
 {
 }
 
@@ -19,8 +20,6 @@ void CTitleProduction::Update()
 	//カメラの動作.
 	m_Camera->Update();
 
-	//地面を動かす.
-	m_Ground->Update();
 }
 
 //描画関数.
@@ -30,7 +29,7 @@ void CTitleProduction::Draw()
 	m_Camera->Info();
 
 	//地面を描画する.
-	m_Ground->Draw(m_Camera->m_mView, m_Camera->m_mProj, m_Camera->m_Light, m_Camera->m_Camera);
+	m_SpriteObjGround->Draw(m_Camera->m_mView, m_Camera->m_mProj);
 }
 
 //生成関数.
@@ -40,20 +39,32 @@ void CTitleProduction::Create()
 	m_Camera = std::make_shared<CCamera>();
 
 	//地面の生成.
-	m_Ground = std::make_unique<CGround>();
+	m_SpriteGround = std::make_unique<CSprite3D>();
 
 	//スタティックメッシュの生成.
 	m_GroundMesh = std::make_shared<CStaticMesh>();
+
+	//地面オブジェクトの生成.
+	m_SpriteObjGround = std::make_unique<CSpriteObject>();
 
 }
 
 //読み込み関数.
 HRESULT CTitleProduction::LoadData()
 {
+	//スプライトの読み込みサイズ.
+	CSprite3D::SPRITE_STATE GROUND
+	{
+
+	};
+
 	//地面のメッシュの読み込み.
 	m_GroundMesh->Init(_T("Data\\Mesh\\Static\\Ground\\ground.x"));
+	//スプライトの読み込み.
+	m_SpriteGround->Init(CDirectX11::GetInstance(), _T(""), GROUND);
+
 	//地面メッシュのアタッチ.
-	m_Ground->AttachMesh(m_GroundMesh);
+	m_SpriteObjGround->AttachSprite(*m_SpriteGround);
 
 	return S_OK;
 }
--- 3D_Base/Source/SceneManager/SceneBase/GameTitle/TitleProduction/CTitleProduction.h
+++ 3D_Base/Source/SceneManager/SceneBase/GameTitle/TitleProduction/CTitleProduction.h
@@ -3,12 +3,12 @@
 
 //-----外部クラス-----
 #include "Camera//CCamera.h" //カメラクラス.
-#include "GameObject//StaticMeshObject//Ground//CGround.h" //地面クラス.
 #include "GameObject//StaticMeshObject//Character//Player//PlayerManager//CPlayerManager.h"	//プレイヤーマネージャークラス.
+#include "../../../../GameObject/SpriteObject/CSpriteObject.h"	//スプライトオブジェクトクラス.
 
 //-----メッシュクラス-----
 #include "Assets//Mesh//StaticMesh//CStaticMesh.h" //スタティックメッシュクラス.
-
+#include "../../../../Assets/Sprite/Sprite3D/CSprite3D.h" //地面スプライト.
 
 //======================================================
 //	タイトルの演出.
@@ -32,12 +32,14 @@ public:
 	//スタティックメッシュクラス.
 	std::shared_ptr<CStaticMesh>	m_GroundMesh;		//地面メッシュ.
 	std::shared_ptr<CStaticMesh>	m_PlayerMesh;		//プレイヤーメッシュ.
+	//スプライトクラス.
+	std::unique_ptr<CSprite3D>		m_SpriteGround;
 
-	//カメラクラス変数.
+	//カメラクラス.
 	std::shared_ptr<CCamera>		m_Camera;
-	//地面クラス変数.
-	std::unique_ptr<CGround>		m_Ground;
-	//プレイヤークラス変数.
+	//地面クラス.
+	std::unique_ptr<CSpriteObject>	m_SpriteObjGround;
+	//プレイヤークラス.
 	std::unique_ptr<CPlayerManager> m_Player;
 
 };
