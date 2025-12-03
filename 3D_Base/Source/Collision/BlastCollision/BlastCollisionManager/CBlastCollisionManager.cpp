d1f9fb6f8e33ce3d7e60f16e840601ecadaa205f
Matsuoka <2400005@i-seifu.ac.jp>
2025-12-03 18:23:05 +0900

    iranai

--- 3D_Base/Source/Collision/BlastCollision/BlastCollisionManager/CBlastCollisionManager.cpp
+++ 3D_Base/Source/Collision/BlastCollision/BlastCollisionManager/CBlastCollisionManager.cpp
@@ -26,7 +26,6 @@ void CBlastCollisionManager::Draw(D3DXMATRIX& View, D3DXMATRIX& Proj, LIGHT& Lig
 {
 	for (auto& blast : m_pBlastCollision)
 	{
-		blast->SetScale(blast->GetBlastRadius());
 		blast->Draw(View, Proj, Light, Camera);
 	}
 }
