78cbe16f0171f18f643ed710ecf95d0df8155154
Matsuoka <2400005@i-seifu.ac.jp>
2025-12-11 14:27:49 +0900

    タイマーの表示時間を変更中

---
+++ 3D_Base/Source/GameObject/UI/CUIObject/NumberImage/NumberImage.h
@@ -0,0 +1,18 @@
+#pragma once
+//-----ライブラリ-----
+#include <iostream>
+#include <unordered_map>
+
+
+//=========================================================
+//				数字画像を管理するクラス.
+//=========================================================
+class NumberImage
+{
+public:
+	NumberImage();
+	~NumberImage();
+
+private:
+
+};
