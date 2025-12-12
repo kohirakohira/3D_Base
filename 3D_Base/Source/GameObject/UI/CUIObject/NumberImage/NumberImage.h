54c5e1e0be363478e876bcc902998a0eac26fd1e
Matsuoka <2400005@i-seifu.ac.jp>
2025-12-12 13:31:37 +0900

    数字画像の変更終了

--- 3D_Base/Source/GameObject/UI/CUIObject/NumberImage/NumberImage.h
+++ 3D_Base/Source/GameObject/UI/CUIObject/NumberImage/NumberImage.h
@@ -1,6 +1,7 @@
 #pragma once
 //-----ライブラリ-----
 #include <iostream>
+#include <vector>
 #include <unordered_map>
 #include <algorithm>
 #include <string>
@@ -8,8 +9,6 @@
 //-----継承-----
 #include "GameObject//UI/CUIObject/CUIObject.h"
 
-//-----必要クラス-----
-#include "Assets//Sprite/Sprite2D/CSprite2D.h"
 
 //=========================================================
 //				数字画像を管理するクラス.
@@ -17,18 +16,6 @@
 class NumberImage
 	: public CUIObject
 {
-public:
-	//.
-
-
-	//数字構造体.
-	struct NUM
-	{
-		int Thousand;	//千の位.
-		int Hundred;	//百の位.
-		int Ten;		//十の位.
-		int One;		//一の位.
-	};
 public:
 	NumberImage();
 	~NumberImage() override;
@@ -40,39 +27,22 @@ public:
 	//初期化.
 	void Init();
 
-	//数字の計算.
-	void NumberCalculations();
-
-	//画像の設定.
-	std::shared_ptr<CSprite2D> SetSpriteNumber(std::shared_ptr<CSprite2D> img) { m_NumImage = img; }
-
-	//制限時間の設定.
-	float SetNumber(float num) { m_Number = num; }
-
 	//数字の設定.
-	void SetNumber(int num)
-	{
-		if (num < 0 || num > 9)
-		{
-			num = 0;
-		}
-		else
-		{
-			m_PatternNo.x = num;
-			m_PatternNo.y = 0;
-		}
-	}
+	void SetNumber(int num, int digitsNum);
 
+	//画像の表示幅の設定.
+	void SetDigitWidth(float w) { m_DigitWidth = w; }
 
 public:
-	//数字.
-	int m_Number;
+	//桁ごとのUI.
+	std::vector<std::shared_ptr<CUIObject>>	m_DigitObject;
 	//UV設定用.
-	std::unordered_map<std::string, std::shared_ptr<NumberImage>>	m_NumBerMap;
-	//画像.
-	std::shared_ptr<CSprite2D>		m_NumImage;
+	std::unordered_map<int, POINT>	m_DigitPattern_Map;
+	//桁ごとに分解した数字.
+	std::vector<int>				m_Digits;
+	//画像の横幅.
+	float							m_DigitWidth;
+	//基準位置.
+	D3DXVECTOR2						m_BasePos;
 
-private:
-	//数字構造体の変数.
-	NUM m_Num;
 };
