b6c4ffa1af92b7ec6d58744d12db3a235ca9b2ee
Matsuoka <2400005@i-seifu.ac.jp>
2026-01-08 12:00:00 +0900

    時計の画像編集をした

--- 3D_Base/Source/SceneManager/SceneBase/GameMain/CGameMain.cpp
+++ 3D_Base/Source/SceneManager/SceneBase/GameMain/CGameMain.cpp
@@ -15,7 +15,7 @@ static bool prevA = false;
 #include "GameObject/StaticMeshObject/Character/CharacterObject/CCharacterObject.h"
 
 //定数宣言.
-static constexpr int TIME = 10;		//時間は調整してください.
+static constexpr int TIME = 90;		//時間は調整してください.
 const float deltaTime = 1.0f / FPS;
 const float DIALMETER = 360.0f;			//時計の回転する針に使用.
 const float FLASH_TIME = 0.5f;			//点滅周期.
